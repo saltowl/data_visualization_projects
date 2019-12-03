3d495c02a0852fef19d335f8895764cc58936359
saltowl <vladislava-shakirova@mail.ru>
2019-12-03 19:19:16 +0300

    Add functionality according to the task

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -19,6 +19,8 @@ class BarChart extends React.Component {
         this.getData = this.getData.bind(this);
         this.updateChart = this.updateChart.bind(this);
         this.updateScales = this.updateScales.bind(this);
+        this.handleMouseOverBar = this.handleMouseOverBar.bind(this);
+        this.handleMouseOutBar = this.handleMouseOutBar.bind(this);
 
         this.getData();
     }
@@ -34,8 +36,9 @@ class BarChart extends React.Component {
 
                 this.setState({
                     linearScale: d3.scaleLinear()
-                        .domain([0, d3.max(GDP)])
+                        .domain([0, d3.max(GDP) + 1000])
                         .range([0, this.props.height]),
+                    rawDates: data.data.map(d => d[0]),
                     dates: data.data.map(d => new Date(d[0])),
                     GDP,
                     barWidth: this.props.width / GDP.length
@@ -51,7 +54,7 @@ class BarChart extends React.Component {
     updateChart() {
         if (this.state.error || !this.state.GDP) return;
 
-        const { dates, GDP, barWidth } = this.state;
+        const { dates, GDP, barWidth, rawDates } = this.state;
         const { height, margin } = this.props;
 
         this.updateScales();
@@ -65,7 +68,9 @@ class BarChart extends React.Component {
             .attr('x', (d, i) => this.state.xScale(dates[i]) + margin)
             .attr('y', (d, i) => height - d)
             .attr('width', barWidth)
-            .attr('height', d => d);
+            .attr('height', d => d)
+            .attr('data-date', (d, i) => rawDates[i])
+            .attr('data-gdp', (d, i) => GDP[i]);
 
         const xAxisGroup = d3.select(this.viz)
             .select('#x-axis')
@@ -95,17 +100,41 @@ class BarChart extends React.Component {
         this.setState({xScale, yScale});
     }
 
+    handleMouseOverBar(e) {
+        const tooltip = d3.select('#tooltip');
+        const bar = d3.select(e.target);
+        const gdp = parseFloat(bar.attr('data-gdp'));
+        const date = bar.attr('data-date');
+        const left = e.target.x.baseVal.value;
+        const top = this.props.height - this.props.margin;
+
+        tooltip.transition()
+            .duration(200)
+            .style('opacity', 1);
+
+        tooltip.html(`${ date } <br> $ ${ gdp.toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') } Billion`)
+            .attr('data-date', date)
+            .style('left', `${ left }px`)
+            .style('top', `${ top }px`)
+            .style('transform', 'translateX(10px)');
+    }
+
+    handleMouseOutBar(e) {
+        d3.select('#tooltip')
+            .transition()
+            .duration(200)
+            .style('opacity', 0);
+    }
+
     render() {
         const { error, GDP, barWidth } = this.state;
         const { width, height, margin } = this.props;
 
         const bars = GDP 
-            ? GDP.map((d, i) => (<rect key={`bar${i}`} className='bar' y={ height } x={ margin + i * barWidth } />)) 
+            ? GDP.map((d, i) => (<rect key={`bar${i}`} className='bar' 
+                onMouseOver={ this.handleMouseOverBar } onMouseOut={ this.handleMouseOutBar } 
+                y={ height } x={ margin + i * barWidth } />)) 
             : [];
-
-
-        const xAxis = <g id='x-axis' transform={`translate(${margin}, ${height})`} />;
-        const yAxis = <g id='y-axis' transform={`translate(${margin}, 0)`} />;
 
         return (
             <div className='main'>
@@ -114,10 +143,11 @@ class BarChart extends React.Component {
                     ? <Error message={ error.message } /> 
                     : <div className='viz'>
                         <div id='title'>United States GDP</div>
+                            <div id='tooltip' />
                             <svg ref={ viz => (this.viz = viz) } width={ width + 100 } height={ height + margin }>
+                                <g id='x-axis' transform={ `translate(${margin}, ${height})` } />
+                                <g id='y-axis' transform={ `translate(${margin}, 0)` } />
                                 { bars }
-                                { xAxis }
-                                { yAxis }
                             </svg>
                         </div> 
                     }
