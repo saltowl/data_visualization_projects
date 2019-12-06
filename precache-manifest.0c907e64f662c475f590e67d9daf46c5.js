12dc08d5b98f76ae49bafab952ab53ef1ce735ea
saltowl <vladislava-shakirova@mail.ru>
2019-12-06 13:40:49 +0300

    Updates

---
+++ precache-manifest.0c907e64f662c475f590e67d9daf46c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2585511eaaa2c0e43896cdab05ed8427",
+    "url": "/data_visualization_projects/index.html"
+  },
+  {
+    "revision": "34e6a74f5c9e73a86174",
+    "url": "/data_visualization_projects/static/css/main.69311ea6.chunk.css"
+  },
+  {
+    "revision": "da1c0410b6aab581f1c1",
+    "url": "/data_visualization_projects/static/js/2.74d19abd.chunk.js"
+  },
+  {
+    "revision": "34e6a74f5c9e73a86174",
+    "url": "/data_visualization_projects/static/js/main.a51c4646.chunk.js"
+  },
+  {
+    "revision": "13f4ad38f7918e36c752",
+    "url": "/data_visualization_projects/static/js/runtime~main.ba3a02e7.js"
+  }
+]);
